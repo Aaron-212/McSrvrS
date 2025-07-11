a0d02b289ca6a558e4f0c405453a65b9eec47157
Aaron Ruan <i@ar212.com>
2025-07-11 11:30:31 +0800

    Initial Commit

---
+++ mc-server-status/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  mc-server-status
+//
+//  Created by Aaron212 on 2025-07-11.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
